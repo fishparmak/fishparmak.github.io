e172d429a7c33a483795cc8d226ebe26b939ae10
danabanana <dana140198@gmail.com>
2021-03-16 20:59:35 +0600

    feat: add firebase

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,12 +1,15 @@
+mportScripts("https://www.gstatic.com/firebasejs/7.16.1/firebase-app.js");
+importScripts(
+    "https://www.gstatic.com/firebasejs/7.16.1/firebase-messaging.js",
+);
+// For an optimal experience using Cloud Messaging, also add the Firebase SDK for Analytics.
+importScripts(
+    "https://www.gstatic.com/firebasejs/7.16.1/firebase-analytics.js",
+);
 
-    MsgElem = document.getElementById("msg");
-    TokenElem = document.getElementById("token");
-    NotisElem = document.getElementById("notis");
-    ErrElem = document.getElementById("err");
-
-    // Initialize Firebase
-    // TODO: Replace with your project's customized code snippet
-    var config = {
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
+firebase.initializeApp({
     apiKey: "AIzaSyCuqhSq393tr6QMZdpJKu9_BcYrfbsed3o",
     authDomain: "mycarpro-e91d1.firebaseapp.com",
     projectId: "mycarpro-e91d1",
@@ -14,58 +17,48 @@
     messagingSenderId: "204195380911",
     appId: "1:204195380911:web:f50b325f9733660abc1e5a",
     measurementId: "G-1SXKVQEQWG"
-};
-    firebase.initializeApp(config);
-
-    const messaging = firebase.messaging();
-    messaging
-    .requestPermission()
-    .then(function () {
-    MsgElem.innerHTML = "Notification permission granted."
-    console.log("Notification permission granted.");
-
-    // get the token in the form of promise
-    return messaging.getToken()
-})
-    .then(function(token) {
-    TokenElem.innerHTML = "token is : " + token
-    sendTokenToServer(currentToken);
-})
-    .catch(function (err) {
-    ErrElem.innerHTML =  ErrElem.innerHTML + "; " + err
-    console.log("Unable to get permission to notify.", err);
 });
 
-    let enableForegroundNotification = true;
-    messaging.onMessage(function(payload) {
-    console.log("Message received. ", payload);
-    NotisElem.innerHTML = NotisElem.innerHTML + JSON.stringify(payload);
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
 
-    if(enableForegroundNotification) {
-    const {title, ...options} = JSON.parse(payload.data.notification);
-    navigator.serviceWorker.getRegistrations().then(registration => {
-    registration[0].showNotification(title, options);
-});
-}
-});
+messaging.setBackgroundMessageHandler(function(payload) {
+    console.log(
+        "[firebase-messaging-sw.js] Received background message ",
+        payload,
+    );
+    // Customize notification here
+    const notificationTitle = "Background Message Title";
+    const notificationOptions = {
+        body: "Background Message body.",
+        icon: "/itwonders-web-logo.png",
+    };
 
-    // отправка ID на сервер
-    function sendTokenToServer(currentToken) {
-    if (!isTokenSentToServer(currentToken)) {
-    console.log('Отправка токена на сервер...');
-
-    var url = 'notification-token'; // адрес скрипта на сервере который сохраняет ID устройства
-    $.ajaxSetup({
-    headers:{
-    'Authorization': "1c68140c1a43af4bbea83860cc33ca80"
-}
-});
-    $.post(url, {
-    token: currentToken
+    return self.registration.showNotification(
+        notificationTitle,
+        notificationOptions,
+    );
 });
 
-    setTokenSentToServer(currentToken);
-} else {
-    console.log('Токен уже отправлен на сервер.');
-}
-}
+
+//     // отправка ID на сервер
+//     function sendTokenToServer(currentToken) {
+//     if (!isTokenSentToServer(currentToken)) {
+//     console.log('Отправка токена на сервер...');
+//
+//     var url = 'notification-token'; // адрес скрипта на сервере который сохраняет ID устройства
+//     $.ajaxSetup({
+//     headers:{
+//     'Authorization': "1c68140c1a43af4bbea83860cc33ca80"
+// }
+// });
+//     $.post(url, {
+//     token: currentToken
+// });
+//
+//     setTokenSentToServer(currentToken);
+// } else {
+//     console.log('Токен уже отправлен на сервер.');
+// }
+// }
